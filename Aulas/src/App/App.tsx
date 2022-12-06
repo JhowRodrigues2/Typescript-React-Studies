97ea0577ef2b2673fd2640e17364357954842494
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-12-06 18:21:19 -0300

    add dashboard and react router dom

--- Aulas/src/App/App.tsx
+++ Aulas/src/App/App.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
+import { Routes } from "./routes";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return <Routes />;
 }
 
 export default App;
